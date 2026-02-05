d9035dc810f7150cba4d78f0ae2b48cf3a029233
AugustoSalicios <gutorodrigues2003@yahoo.com.br>
2026-02-05 14:11:38 -0300

    Update dbService.ts
    Por alguma razão não estava conseguindo corrigir remotamente, então tive q vir aqui corrigir

--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
     password: process.env.PASSWORD,
     port: Number(process.env.DB_PORT)
 })
